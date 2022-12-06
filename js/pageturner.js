dffbf946b46c91817ca19cda694ba03bf48b7448
RichardRNS <71282342+RichardRNS@users.noreply.github.com>
2022-12-05 23:27:35 -0400

    Add files via upload

---
+++ js/pageturner.js
@@ -0,0 +1,42 @@
+$(window).on("load", function(){
+
+    pages=[".deux", ".une"]; //Add more as I write more
+
+    var cnt = 0; //Page count
+
+    hideAll();
+    $("#next").on("click", nextPage);
+    $("#prev").on("click", prevPage);
+
+    function hideAll(){ //Hide all pages except P1
+        for (var i = 1; i < pages.length; i++){
+            $(pages[i]).hide();
+        }
+    };
+
+    function nextPage(){ //Turn to next page
+        if (cnt < (pages.length-1)){ //If not on last page
+            $(pages[cnt]).hide();
+            cnt++;
+            $(pages[cnt]).fadeIn("fast");
+            $(window).scrollTop(0);
+        }
+        else {
+            window.alert("You're already on the last page.");
+        };
+    };
+
+    function prevPage(){//Turn to previous page
+        if (cnt > 0){ //If not on last page
+            $(pages[cnt]).hide();
+            cnt--;
+            $(pages[cnt]).fadeIn("fast");
+            $(window).scrollTop(0);
+        }
+        else {
+            window.alert("You're already on the first page.");
+        };
+    };
+
+
+});
